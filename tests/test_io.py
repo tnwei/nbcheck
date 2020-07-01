63bf77548f0406dc8a1107fe5ff92ac0b399b9c5
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-07-01 21:13:18 +0800

    Added test cases for TDD

---
+++ tests/test_io.py
@@ -0,0 +1,13 @@
+def test_multiple_files():
+    """
+    Tests that multiple files can be processed
+    """
+    pass
+
+
+def test_expansion():
+    """
+    Tests that wildcard expansion works when passing in files
+    """
+    pass
+
---
+++ tests/test_usage.py
@@ -0,0 +1,26 @@
+def test_notebook_in_order():
+    """
+    Tests that a notebook w/ all code cells executed in
+    order is determined so correctly.
+    """
+    pass
+
+def test_notebook_not_in_order():
+    """
+    Tests that a notebook with cells not in order are
+    detected correctly.
+    """
+    pass
+
+def test_no_code_cells():
+    """
+    Tests the edge case where a notebook has no code cells.
+    """
+    pass
+
+def test_not_a_notebook():
+    """
+    Raises errors when files that are not Jupyter notebooks
+    are passed.
+    """
+    pass
