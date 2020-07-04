c068f73b7a4078640618a974a862241617de08a4
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-07-04 14:16:18 +0800

    Wrapped nbcheck in function for testing

--- nbcheck.py
+++ nbcheck.py
@@ -4,37 +4,73 @@
 import sys
 import json
 
-# Load notebook(s)
-nbdir = sys.argv[1]
+def nbcheck(nbdirs):
+    """
+    Given a list of directories pointing to Jupyter notebooks, checks that code cells
+    are run in order.
 
-with open(nbdir, "r") as f:
-    file = json.load(f)
+    Usage
+    -----
+    >>> nbdirs = ["notebook1.ipynb", "notebook2.ipynb"]
+    >>> nbcheck(nbdirs)
 
-# Retrieve cells only as a list
-cells = file["cells"]
+    Input
+    -----
+    nbdirs: list
+        List of directories / PathLikes.
+        Note `open()` wasn't able to handle PathLikes until the ability was introduced in
+        Python 3.6 via [PEP 519](https://www.python.org/dev/peps/pep-0519/)
 
-# Preserve only code cells
-cells = [i for i in cells if i["cell_type"] == "code"]
-num_cells = len(cells)
+    Output
+    ------
+    results: dict
+        Returns dict w/ format {nbdir: cells_out_of_order} for testing purposes
+    """
+    results = {}
 
-# Remove null code cells, raise warnings on them though!
-cells = [i for i in cells if isinstance(i["execution_count"], int)]
+    for nbdir in nbdirs:
+        # Load notebook
+        with open(nbdir, "r") as f:
+            file = json.load(f)
 
-if num_cells - len(cells) != 0:
-    print(f"Warning: {num_cells - len(cells)} code cells are not run!")
-    num_cells = len(cells)
+        # Retrieve cells only as a list
+        cells = file["cells"]
 
-# Check that all cells are sorted
-cells_out_of_order = []
+        # Preserve only code cells
+        cells = [i for i in cells if i["cell_type"] == "code"]
+        num_cells = len(cells)
 
-for i in range(1, num_cells):
-    # Use less than since all `execution_count` numbers are unique
-    if cells[i]["execution_count"] < cells[i-1]["execution_count"]:
-        cells_out_of_order.append(i)
-        print(f"Cell {i} is out of order!")
+        # Remove code cells that are not run and raise warnings
+        cells = [i for i in cells if isinstance(i["execution_count"], int)]
 
-# Print closing message
-if len(cells_out_of_order) == 0:
-    print(f"{nbdir} has all cells executed in order!")
-else:
-    print(f"{nbdir} has {len(cells_out_of_order)} cells ran out of order!")
+        if num_cells - len(cells) != 0:
+            print(f"{nbdir}: Warning: {num_cells - len(cells)} code cells are not run!")
+            num_cells = len(cells)
+
+        # Check that all cells are sorted
+        cells_out_of_order = []
+
+        for i in range(1, num_cells):
+            # Use less than since all `execution_count` numbers are unique
+            if cells[i]["execution_count"] < cells[i-1]["execution_count"]:
+                cells_out_of_order.append(i)
+
+        # Track results
+        results[nbdir] = cells_out_of_order
+
+        # Print closing message
+        if len(cells_out_of_order) == 0:
+            print(f"{nbdir}: All cells executed in order!")
+        else:
+            print(f"{nbdir}: {len(cells_out_of_order)} cells executed out of order!")
+            print(f"{nbdir}: Cells: {cells_out_of_order}")
+        
+    return results
+
+
+if __name__ == "__main__":
+    # Retrieve list of notebook(s)
+    nbdirs = sys.argv[1:]
+
+    # Run check
+    nbcheck(nbdirs)
