929d84fa3ab8e3b56b3b08e1a49763fd9dc273c8
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-07-04 14:27:11 +0800

    Corrected path

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,13 +1,15 @@
 from nbcheck import nbcheck
 from pathlib import Path
 
+fdir = Path(__file__).parent
+
 def test_notebook_in_order():
     """
     Tests that a notebook w/ all code cells executed in
     order is determined so correctly.
     """
-    fdir = [Path(".") / "cells-in-order.ipynb"]
-    results = nbcheck(fdir)
+    nbdirs = [fdir / "cells-in-order.ipynb"]
+    results = nbcheck(nbdirs)
     print(results)
 
 
@@ -16,8 +18,8 @@ def test_notebook_not_in_order():
     Tests that a notebook with cells not in order are
     detected correctly.
     """
-    fdir = [Path(".") / "cells-not-in-order.ipynb"]
-    results = nbcheck(fdir)
+    nbdirs = [fdir / "cells-not-in-order.ipynb"]
+    results = nbcheck(nbdirs)
     print(results)
 
 
@@ -25,8 +27,8 @@ def test_no_code_cells():
     """
     Tests the edge case where a notebook has no code cells.
     """
-    fdir = [Path(".") / "no-code-cells.ipynb"]
-    results = nbcheck(fdir)
+    nbdirs = [fdir / "no-code-cells.ipynb"]
+    results = nbcheck(nbdirs)
     print(results)
 
 
@@ -35,6 +37,6 @@ def test_not_a_notebook():
     Raises errors when files that are not Jupyter notebooks
     are passed.
     """
-    fdir = [Path(".") / "not-a-notebook.txt"]
-    results = nbcheck(fdir)
+    nbdirs = [fdir / "not-a-notebook.txt"]
+    results = nbcheck(nbdirs)
     print(results)
