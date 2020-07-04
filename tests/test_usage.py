e773bcb7054010aacb559ba7b316dcf8ec4743c3
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-07-04 14:17:12 +0800

    Filled out test_usage.py

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,26 +1,40 @@
+from nbcheck import nbcheck
+from pathlib import Path
+
 def test_notebook_in_order():
     """
     Tests that a notebook w/ all code cells executed in
     order is determined so correctly.
     """
-    pass
+    fdir = [Path(".") / "cells-in-order.ipynb"]
+    results = nbcheck(fdir)
+    print(results)
+
 
 def test_notebook_not_in_order():
     """
     Tests that a notebook with cells not in order are
     detected correctly.
     """
-    pass
+    fdir = [Path(".") / "cells-not-in-order.ipynb"]
+    results = nbcheck(fdir)
+    print(results)
+
 
 def test_no_code_cells():
     """
     Tests the edge case where a notebook has no code cells.
     """
-    pass
+    fdir = [Path(".") / "no-code-cells.ipynb"]
+    results = nbcheck(fdir)
+    print(results)
+
 
 def test_not_a_notebook():
     """
     Raises errors when files that are not Jupyter notebooks
     are passed.
     """
-    pass
+    fdir = [Path(".") / "not-a-notebook.txt"]
+    results = nbcheck(fdir)
+    print(results)
